814dc922a050bf94d2206c7c6af4eca83829402c
Sheep26 <crabeykiller@gmail.com>
2024-04-29 23:04:26 +1200

    only get battery power

--- power_monitor_plugin.sh
+++ power_monitor_plugin.sh
@@ -6,25 +6,25 @@ ICON=utilities-system-monitor
 POWERSUPPLIES=$(ls '/sys/class/power_supply/' | tr " " "\n")
 VOLTAGE=""
 CURRENT=""
-USINGAC=false
+#USINGAC=false
 
 # Cycle though each powersupply to figure out which powersupply to use
 for POWERSUPPLY in $POWERSUPPLIES; do
     # Find out which powersupply to use
-    if [[ "$POWERSUPPLY" == AC* ]]; then
+    #if [[ "$POWERSUPPLY" == AC* ]]; then
         # Check if plugged into mains
-        for DATA in $(cat "/sys/class/power_supply/$POWERSUPPLY/uevent"); do
-            if [[ "$DATA" == POWER_SUPPLY_ONLINE=1 ]]; then
-                VOLTAGE=10000000000000
-                CURRENT=10000000000
-                USINGAC=true
-                break
-            fi
-        done
-        if "$USINGAC"; then
-            break
-        fi
-    fi
+        #for DATA in $(cat "/sys/class/power_supply/$POWERSUPPLY/uevent"); do
+            #if [[ "$DATA" == POWER_SUPPLY_ONLINE=1 ]]; then
+                #VOLTAGE=10000000000000
+                #CURRENT=10000000000
+                #USINGAC=true
+                #break
+            #fi
+        #done
+        #if "$USINGAC"; then
+            #break
+        #fi
+    #fi
     if [[ "$POWERSUPPLY" == BAT* ]]; then
         VOLTAGE=$(cat "/sys/class/power_supply/$POWERSUPPLY/voltage_now")
         CURRENT=$(cat "/sys/class/power_supply/$POWERSUPPLY/current_now")
@@ -33,9 +33,11 @@ for POWERSUPPLY in $POWERSUPPLIES; do
 done
 
 # Get wattage drawn
-WATTAGE=$(((VOLTAGE * CURRENT) / 1000000000000))
+WATTAGE=$(echo $(((VOLTAGE * CURRENT) / 10000000000)) | sed -e 's/..$/.&/;t' -e 's/.$/.0&/')
 
 # do the genmon
 echo "<txt>$WATTAGE‎W</txt>"
+echo "<tool>Voltage | $(echo $((VOLTAGE / 10000)) | sed -e 's/..$/.&/;t' -e 's/.$/.0&/')
+Current | $(echo $((CURRENT / 10000)) | sed -e 's/..$/.&/;t' -e 's/.$/.0&/')</tool>"
 
 exit 0
