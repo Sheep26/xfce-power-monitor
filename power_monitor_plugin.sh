cc489d33a0942967fc677af9917b3b7780063325
Sheep26 <crabeykiller@gmail.com>
2024-04-29 21:00:17 +1200

    Added script

---
+++ power_monitor_plugin.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+ICON=utilities-system-monitor
+
+# Get the target powersupply
+POWERSUPPLIES=$(ls '/sys/class/power_supply/' | tr " " "\n")
+VOLTAGE=""
+CURRENT=""
+
+# Cycle though each powersupply to figure out which powersupply to use
+for POWERSUPPLY in $POWERSUPPLIES; do
+    # Find out which powersupply to use
+    if [[ "$POWERSUPPLY" == AC* ]]; then
+        # Check if mains plugged in
+        for DATA in [[ $(cat '/sys/class/power_supply/ACAD/uevent') ]]
+        do
+            if DATA == "POWER_SUPPLY_ONLINE=1"; then
+                VOLTAGE = 10000000000000
+                CURRENT = 10000000000
+                break
+            fi
+        done
+    fi
+    if [[ "$POWERSUPPLY" == BAT* ]]; then
+        VOLTAGE=$(cat "/sys/class/power_supply/$POWERSUPPLY/voltage_now")
+        CURRENT=$(cat "/sys/class/power_supply/$POWERSUPPLY/current_now")
+        break
+    fi
+done
+
+# Get wattage drawn
+WATTAGE=$(((VOLTAGE * CURRENT) / 1000000000000))
+
+# do the genmon
+echo "<txt>$WATTAGE watts</txt><txtclick>xfce4-power-manager</txtclick>"
+
+exit 0
