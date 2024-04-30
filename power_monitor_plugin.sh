064906cc009e4ad76b8e96440bf8c63fbf3e0959
Sheep26 <crabeykiller@gmail.com>
2024-04-30 22:20:39 +1200

    cleaner code

--- power_monitor_plugin.sh
+++ power_monitor_plugin.sh
@@ -6,26 +6,41 @@ ICON=utilities-system-monitor
 POWERSUPPLIES=$(ls '/sys/class/power_supply/' | tr " " "\n")
 VOLTAGE=0
 CURRENT=0
+NODATA=false
 
 # Cycle though each powersupply to figure out which powersupply to use
 for POWERSUPPLY in $POWERSUPPLIES; do
-    # Find out which powersupply to use
-    if [[ "$POWERSUPPLY" == AC* ]]; then
-        # Check if plugged into mains
-        if [[ $(cat "/sys/class/power_supply/$POWERSUPPLY/online") == "1" ]]; then
-            #if [ -f "/sys/class/power_supply/$POWERSUPPLY/voltage_now" ] && [ -f "/sys/class/power_supply/$POWERSUPPLY/current_now" ]; then     
-            VOLTAGE=$(cat "/sys/class/power_supply/$POWERSUPPLY/voltage_now")
-            CURRENT=$(cat "/sys/class/power_supply/$POWERSUPPLY/current_now")
-            break
-            #fi
-        fi
+    # Check if power supply online
+    if [[ $(cat "/sys/class/power_supply/$POWERSUPPLY/online") == "0" ]]; then
+        continue
     fi
+
+    # Check if power supply sending voltage and current data to pc
+    if [ ! -f "/sys/class/power_supply/$POWERSUPPLY/voltage_now" ] || [ ! -f "/sys/class/power_supply/$POWERSUPPLY/current_now" ]; then
+        NODATA=true
+        break
+    fi
+
+    # Read data from power supply
+    if [[ "$POWERSUPPLY" == AC* ]]; then
+        VOLTAGE=$(cat "/sys/class/power_supply/$POWERSUPPLY/voltage_now")
+        CURRENT=$(cat "/sys/class/power_supply/$POWERSUPPLY/current_now")
+        NODATA=false
+        break
+    fi
+
     if [[ "$POWERSUPPLY" == BAT* ]]; then
         VOLTAGE=$(cat "/sys/class/power_supply/$POWERSUPPLY/voltage_now")
         CURRENT=$(cat "/sys/class/power_supply/$POWERSUPPLY/current_now")
+        NODATA=false
         break
     fi
 done
+
+if [[ $NODATA == true ]]; then
+    echo "<txt>No data</txt>"
+    exit 0
+fi
 
 # Get wattage drawn
 WATTAGE=$(echo $(((VOLTAGE * CURRENT) / 10000000000)) | sed -e 's/..$/.&/;t' -e 's/.$/.0&/')
